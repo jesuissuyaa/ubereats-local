58cd542e78d00979e2cdbf8b9d30025e5646b8f3
jesuissuyaa <oriko.harada@uec.ac.jp>
2020-05-29 01:44:25 +0900

    cleanup

--- src/components/article-card.js
+++ src/components/article-card.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import { Box, Heading, Stack, Text } from "@chakra-ui/core"
+import { Box, Heading, Text } from "@chakra-ui/core"
 
 const ArticleCard = ({ edge }) => {
   return (
